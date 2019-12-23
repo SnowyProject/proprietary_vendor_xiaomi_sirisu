b3e9ea7e4d3f0f6f365a3cd5c123eeb70baccbab
LuK1337 <priv.luk@gmail.com>
2019-12-23 22:38:41 +0100

    sirius: Import aptX blobs from crosshatch
    * from crosshatch-user 10 QP1A.190711.020 5800535 release-keys
    
    Change-Id: I3d31b3721a34d161d56a6cc232fe28093c3de820

--- sirius/sirius-vendor.mk
+++ sirius/sirius-vendor.mk
@@ -130,6 +130,8 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sirius/proprietary/product/lib64/lib-imsvt.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/lib-imsvt.so \
     vendor/xiaomi/sirius/proprietary/product/lib64/lib-imsvtextutils.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/lib-imsvtextutils.so \
     vendor/xiaomi/sirius/proprietary/product/lib64/lib-imsvtutils.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/lib-imsvtutils.so \
+    vendor/xiaomi/sirius/proprietary/product/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libaptXHD_encoder.so \
+    vendor/xiaomi/sirius/proprietary/product/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libaptX_encoder.so \
     vendor/xiaomi/sirius/proprietary/product/lib64/libdpmctmgr.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libdpmctmgr.so \
     vendor/xiaomi/sirius/proprietary/product/lib64/libdpmfdmgr.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libdpmfdmgr.so \
     vendor/xiaomi/sirius/proprietary/product/lib64/libdpmframework.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libdpmframework.so \
